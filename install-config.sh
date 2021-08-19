41677141d0fa1324652bc823c7f01ca1a0511215
Carl-Johan Waldeck <calle@waldeck.nu>
2021-08-19 21:47:51 +0200

    Update install script to handle re-run
    Only update paths that does not already exist. At the moment there is
    only one, but there may be more to come.

--- install-config.sh
+++ install-config.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-# Install this containing directory as an include path for git config.
-# Any file in this directory will be included.
-git config --global --add include.path "$SCRIPT_DIR/aliases"
+add_include_path ()
+{
+	# Add include path if it doesn't exist already
+	local include_path=$script_dir/$1
+	if [ -z `git config --global --list | grep $include_path` ]
+	then
+		git config --global --add include.path $include_path
+	fi
+}
+
+add_include_path aliases
